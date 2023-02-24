dda15511a19c9aeb8ededd46907941d70096261c
Trissolo <00tristano00@gmail.com>
2023-02-24 07:43:47 +0100

    Reindent

--- gimpfu/room_things.py
+++ gimpfu/room_things.py
@@ -34,13 +34,13 @@ import json
 def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = "_"):
     pdb.gimp_image_undo_group_start(image)
     pdb.gimp_context_push()
-    
+
     #PUT YOUR CODE HERE:
     def showMessage(string):
         return pdb.gimp_message("\n" + string)
-    
+
     #showMessage("Room Things here! :)")
-    
+
     depthCategories = {
     "bg": -10, # BACKGROUND
     "rf": 0, # RIDICULOUSLYFARAWAY
@@ -62,30 +62,30 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
 
     prop_width = "width"
     prop_height = "height"
-    
+
     main_container = {}
 
     things = []
     main_container['things'] = things
     main_container['id'] = room_id
     main_container['atlas'] = atlas_id
-    
+
     #showMessage(json.dumps(main_container))
-    
-    
-    
+
+
+
     def test_names(curr_string, sep = separator):
         for allowed in depthCategories.keys():
             if curr_string.startswith(allowed + sep):
                 return True
         return False
-    
+
     def pre_test(ary = image.layers):
         for _, elem in enumerate(ary):
             if elem.visible and test_names(elem.name):
                 return True
         return False
-    
+
     def middle_down_origin(layer, depth, frame):
         x, y, width, height = get_rect(layer)
         container = {}
@@ -103,7 +103,7 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
         container[prop_depth] = depth
         container[prop_frame] = frame
         return container
-    
+
     def trigger_area(layer, depth, frame):
         x, y, width, height = get_rect(layer)
         container = {}
@@ -114,11 +114,11 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
         container[prop_depth] = depth
         container['name'] = frame
         return container
-        
+
     def room_background(layer, depth, frame):
         main_container['background'] = frame
-        
-        
+
+
 
     #util
     def get_rect(layer):
@@ -131,10 +131,6 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
     #test_message = prop_x+separator+prop_y+separator+prop_frame+separator+prop_depth+str(len(image.layers))+"\n"+str(atlas_id)+separator+ str(room_id)
     #test as
     actions = {"bg": room_background, "rf": top_left_origin, "ta": trigger_area, "co": top_left_origin, "ab": top_left_origin, "ds": middle_down_origin, "fg": top_left_origin}
-    idx = 0
-    tlay = image.layers[idx]
-    de, na = tlay.name.split(separator)
-    
 
     for elem in image.layers:
         if elem.visible and test_names(elem.name):
@@ -143,9 +139,9 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
                 actions[depth](elem, depth, name)
             else:
                 things.append(actions[depth](elem, depth, name))
-    
+
     showMessage(json.dumps(main_container, sort_keys = True))
-    
+
     #NO MORE CODE!
     pdb.gimp_context_pop()
     pdb.gimp_image_undo_group_end(image)
@@ -153,14 +149,14 @@ def room_things_to_json(image, layer, room_id, atlas_id, walkable_area_marker = 
     #return
 
 register(
-    "mio_gimp_fu_room_things_to_json",                           
+    "mio_gimp_fu_room_things_to_json",
     "Generate a JSON containing the information obtained from the layers.",
     "Generate a JSON containing the information obtained from the layers.",
     "Trissolo",
     "Trissolo",
     "2023",
     "<Image>/Windows/TRIS/Generate Room Things JSON...", # <----= Menu path
-    "RGB*, GRAY*", 
+    "RGB*, GRAY*",
     [
     (PF_INT, "room_id", "Room ID", 0),
     (PF_INT, "atlas_id", "Atlas ID", 0)
